f74ae8f1e0eca17350c1fbfc8d571eabc5fe78b1
junonurakbar <junonurakbar@gmail.com>
2024-03-06 22:45:26 +0700

    add: 2 outputs (string and error)

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,14 @@
 package go_say_hello
 
-func SayHello(name string) string {
-	return "Henlo Bruh " + name + " '-')/"
+import (
+	"errors"
+	"fmt"
+)
+
+func SayHello(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+	message := fmt.Sprintf("Henlo Bruh %v '-')/", name)
+	return message, nil
 }
