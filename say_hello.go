3fb3e59a9aea00c77e2a102b31d25f968c4a2b0c
junonurakbar <junonurakbar@gmail.com>
2024-03-06 23:14:30 +0700

    added random format

--- say_hello.go
+++ say_hello.go
@@ -3,12 +3,26 @@ package go_say_hello
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func SayHello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Henlo Bruh %v '-')/", name)
+	message := fmt.Sprintf(randomFormat(),name)
 	return message, nil
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	formats := []string{
+		"Hey bruh %v",
+		"Haven't you had brekkie yet?",
+		"JDON my soul %v '-')/",
+	} 
+	// Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
